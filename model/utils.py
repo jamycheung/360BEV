da0c5f2bd88ce7d76c20cf2a15c199fa38938905
Zhifeng Teng <zhifengteng222@gmail.com>
2023-05-01 00:06:35 +0200

    Organize the code

---
+++ model/utils.py
@@ -0,0 +1,18 @@
+import os
+import logging
+import datetime
+
+
+def get_logger(logdir):
+    logger = logging.getLogger("smnet")
+    ts = str(datetime.datetime.now()).split(".")[0].replace(" ", "_")
+    ts = ts.replace(":", "_").replace("-", "_")
+    file_path = os.path.join(logdir, "run_{}.log".format(ts))
+    hdlr = logging.FileHandler(file_path)
+    formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+    hdlr.setFormatter(formatter)
+    logger.addHandler(hdlr)
+    logger.setLevel(logging.INFO)
+    return logger
+
+
